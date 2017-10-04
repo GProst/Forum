703e6c50ca8399046c6761cb187b42ab42df281c
GProst <gprostmail@gmail.com>
2017-10-04 18:44:03 +0300

    Update Message template

--- client/components/templates/Message.jsx
+++ client/components/templates/Message.jsx
@@ -1,8 +1,78 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled, {css} from 'styled-components'
 import {Link} from 'react-router-dom'
 
 import {Routes} from '../../routes'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 50px 0;
+`
+
+const inputCSS = css`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 100%;
+  min-width: 100%;
+  max-height: 500px;
+  border: 1px solid grey;
+  outline: none;
+  
+  ${props => props.disabled && css`
+  opacity: 0.7;
+  background: #ececec;
+  `}
+`
+
+const HeadInput = styled.input`
+  ${inputCSS};
+  height: 50px;
+  font-size: 24px;
+  padding: 10px 20px;
+`
+
+const BodyInput = styled.textarea`
+  ${inputCSS};
+  height: 200px;
+  min-height: 200px;
+  font-size: 16px;
+  padding: 20px;
+`
+
+const Footer = styled.div`
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Button = styled.button`
+  padding: 5px 10px;
+  font-weight: 400;
+  font-size: 18px;
+  cursor: pointer;
+  outline: none;
+  border: 1px solid grey;
+  background: #ececec;
+  
+  ${props => !props.disabled && css`
+  :hover {
+    background: grey;
+    color: white;
+  }
+  `}
+  
+  ${props => props.disabled && css`
+  cursor: not-allowed;
+  opacity: 0.7;
+  background: #ececec;
+  `}
+`
 
 export default class MessageTemplate extends React.Component {
   static propTypes = {
@@ -12,31 +82,70 @@ export default class MessageTemplate extends React.Component {
       body: PropTypes.string.isRequired
     }),
     pending: PropTypes.bool,
-    disabled: PropTypes.bool
+    disabled: PropTypes.bool,
+    type: PropTypes.oneOf(['update', 'create']).isRequired
   }
 
   static defaultProps = {
-    disabled: false
+    disabled: false,
+    type: 'update'
+  }
+
+  state = {
+    headerValue: this.props.message ? this.props.message.header : '',
+    bodyValue: this.props.message ? this.props.message.body : ''
+  }
+
+  onInputChange(field, event) {
+    const {value} = event.target
+    this.setState({
+      [field]: value
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.message) {
+      this.setState({
+        headerValue: nextProps.message.header,
+        bodyValue: nextProps.message.body
+      })
+    }
   }
 
   render() {
     const {message} = this.props
     const disabled = this.props.disabled || this.props.pending
+    const {headerValue, bodyValue} = this.state
 
     return (
-      <div disabled={disabled}>
+      <Wrapper>
         {message
           ? (
             <div>
-              <h1>{message.header}</h1>
-              <hr />
-              <p>{message.body}</p>
-              <Link to={Routes.messagesList}>Go to messages list</Link>
+              <h1>Header:</h1>
+              <HeadInput disabled={disabled} value={headerValue} onChange={this.onInputChange.bind(this, 'headerValue')} />
+              <h2>Body:</h2>
+              <BodyInput disabled={disabled} value={bodyValue} onChange={this.onInputChange.bind(this, 'bodyValue')} />
+              <Footer>
+                <Link to={Routes.messagesList}>Back to messages list</Link>
+                {this.props.type === 'update'
+                  ? (
+                    <Button type='button' disabled={disabled}>
+                      Save changes
+                    </Button>
+                  )
+                  : (
+                    <Button type='button' disabled={disabled}>
+                      Create message
+                    </Button>
+                  )
+                }
+              </Footer>
             </div>
           )
           : <h2>Fetching message data...</h2>
         }
-      </div>
+      </Wrapper>
     )
   }
 }
