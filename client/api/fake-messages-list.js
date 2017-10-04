cbdeb8f0115fe5f46b8a6b43aa974d36c1114016
GProst <gprostmail@gmail.com>
2017-10-04 12:59:06 +0300

    Create initial api service

---
+++ client/api/fake-messages-list.js
@@ -0,0 +1,37 @@
+export default [{
+  id: 1,
+  header: 'Message #1',
+  body: 'This is a body of message #1'
+}, {
+  id: 2,
+  header: 'Message #2',
+  body: 'This is a body of message #2'
+}, {
+  id: 3,
+  header: 'Message #3',
+  body: 'This is a body of message #4'
+}, {
+  id: 4,
+  header: 'Message #4',
+  body: 'This is a body of message #4'
+}, {
+  id: 5,
+  header: 'Message #5',
+  body: 'This is a body of message #5'
+}, {
+  id: 6,
+  header: 'Message #6',
+  body: 'This is a body of message #6'
+}, {
+  id: 7,
+  header: 'Message #7',
+  body: 'This is a body of message #7'
+}, {
+  id: 8,
+  header: 'Message #8',
+  body: 'This is a body of message #8'
+}, {
+  id: 9,
+  header: 'Message #9',
+  body: 'This is a body of message #9'
+}]
---
+++ client/api/index.js
@@ -0,0 +1,28 @@
+import messagesList from './fake-messages-list'
+
+export default {
+  fetchMessages() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => { // TODO: real request
+        resolve(messagesList)
+      }, 2000)
+    })
+      .catch((err) => {
+        throw err
+      })
+  },
+  fetchMessage(id) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => { // TODO: real request
+        resolve({
+          id,
+          header: `Message #${id}`,
+          body: `This is a body of message #${id}`
+        })
+      }, 2000)
+    })
+      .catch((err) => {
+        throw err
+      })
+  }
+}
