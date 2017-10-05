d1d7830f73103e1420befc2a07076834232923b3
GProst <gprostmail@gmail.com>
2017-10-05 14:50:58 +0300

    Create initial SQLite db

---
+++ server/db/create.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const path = require('path')
+const del = require('del')
+
+const initialMessages = require('./initialMessages')
+
+const dbFile = path.join(__dirname, '../messages.sqlite3')
+del.sync([dbFile])
+
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(dbFile)
+
+db.serialize(() => {
+  db.run(`
+  CREATE TABLE Messages (
+    id  INTEGER  PRIMARY KEY  AUTOINCREMENT  NOT NULL,
+    header  TEXT  NOT NULL,
+    body  TEXT  NOT NULL
+  )
+  `)
+
+  const stmt = db.prepare('INSERT INTO Messages (header, body) VALUES (?, ?)')
+  initialMessages.forEach(({header, body}) => {
+    stmt.run(header, body)
+  })
+  stmt.finalize()
+})
+
+db.close()
---
+++ server/db/initialMessages.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = [{
+  header: 'Message #1',
+  body: 'This is a body of message #1'
+}, {
+  header: 'Message #2',
+  body: 'This is a body of message #2'
+}, {
+  header: 'Message #3',
+  body: 'This is a body of message #4'
+}, {
+  header: 'Message #4',
+  body: 'This is a body of message #4'
+}, {
+  header: 'Message #5',
+  body: 'This is a body of message #5'
+}]
