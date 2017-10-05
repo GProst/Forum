936ae9068c97cfbf1b28795e604e2df78412c4eb
GProst <gprostmail@gmail.com>
2017-10-05 19:02:54 +0300

    Update unit tests

--- server/routers/api.router.spec.js
+++ server/routers/api.router.spec.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const expect = require('chai').expect
+const sinon = require('sinon')
+
+const dbManager = require('../db/manager')
 
 describe('Checking API router responses', () => {
   describe('errorHandler() function', () => {
@@ -16,6 +19,18 @@ describe('Checking API router responses', () => {
   })
 
   describe('onMessageGet handler', () => {
+    before(() => {
+      sinon.stub(dbManager, 'getMessage')
+    })
+
+    afterEach(() => {
+      dbManager.getMessage.reset()
+    })
+
+    after(() => {
+      dbManager.getMessage.restore()
+    })
+
     it('SHOULD response JSON with message if message exists', () => {
       expect(2 + 2).to.be.equal(4)
     })
@@ -30,6 +45,18 @@ describe('Checking API router responses', () => {
   })
 
   describe('onMessageUpdate handler', () => {
+    before(() => {
+      sinon.stub(dbManager, 'updateMessage')
+    })
+
+    afterEach(() => {
+      dbManager.updateMessage.reset()
+    })
+
+    after(() => {
+      dbManager.updateMessage.restore()
+    })
+
     it('SHOULD call sendOK() if updating was successful', () => {
       expect(2 + 2).to.be.equal(4)
     })
@@ -40,6 +67,18 @@ describe('Checking API router responses', () => {
   })
 
   describe('onMessageDelete handler', () => {
+    before(() => {
+      sinon.stub(dbManager, 'deleteMessage')
+    })
+
+    afterEach(() => {
+      dbManager.deleteMessage.reset()
+    })
+
+    after(() => {
+      dbManager.deleteMessage.restore()
+    })
+
     it('SHOULD call sendOK() if updating was successful', () => {
       expect(2 + 2).to.be.equal(4)
     })
@@ -50,6 +89,18 @@ describe('Checking API router responses', () => {
   })
 
   describe('onMessageCreate handler', () => {
+    before(() => {
+      sinon.stub(dbManager, 'createMessage')
+    })
+
+    afterEach(() => {
+      dbManager.createMessage.reset()
+    })
+
+    after(() => {
+      dbManager.createMessage.restore()
+    })
+
     it('SHOULD response JSON with "id" field if creating was successful', () => {
       expect(2 + 2).to.be.equal(4)
     })
@@ -60,6 +111,18 @@ describe('Checking API router responses', () => {
   })
 
   describe('onMessagesGet handler', () => {
+    before(() => {
+      sinon.stub(dbManager, 'getAllMessages')
+    })
+
+    afterEach(() => {
+      dbManager.getAllMessages.reset()
+    })
+
+    after(() => {
+      dbManager.getAllMessages.restore()
+    })
+
     it('SHOULD response JSON with "messages" field if messages exist', () => {
       expect(2 + 2).to.be.equal(4)
     })
